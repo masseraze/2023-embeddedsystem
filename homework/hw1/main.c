a2b996ffd2a48dc3286a1a52a6ed6769da249bfd
Masseraze Tung <sujason72@gmail.com>
2023-11-01 03:14:24 +0800

    add hit miss to calculate the proporation

--- homework/hw1/main.c
+++ homework/hw1/main.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<math.h>
 #include "cache_model.h"
 
-#define READ 10000
+#define READ 100000
 enum Flag{a,b,c,d}flag;
+int hit = 0;
+int miss = 0;
 
 float get_statistic_cycle(int *addr){
     float time = 0;
@@ -13,6 +16,11 @@ float get_statistic_cycle(int *addr){
     for(i=0;i<READ;i++){
         cm_do_access(get_address(addr));
         pre_time = cm_get_last_access_cycles();
+        if(pre_time==1){
+            hit++;
+        }else{
+            miss++;
+        }
         time = time + pre_time;
     }
     return time/i;
@@ -28,15 +36,17 @@ int get_address(int *address){
         }else if(p < 0.95){
             int rnd = rand_int(80);
             *address = (rnd<40)?(*address-41+rnd):(*address+81-rnd);
-            *address = (*address>65536)?*address-65536:*address;
+            *address = (*address>65535)?*address-65536:*address;
             *address = (*address<0)?*address+65536:*address;
+            //fprintf(stderr,"[%s ,%d]\n",__func__,__LINE__);
             return *address;
         }
         else{
             int rnd = rand_int(CM_ADDRESS_SPACE_SIZE-84);
             *address = (rnd<(CM_ADDRESS_SPACE_SIZE-84)/2)?(*address -(CM_ADDRESS_SPACE_SIZE)/2+rnd):(*address+CM_ADDRESS_SPACE_SIZE-41-rnd);
-            *address = (*address>65536)?*address-65536:*address;
+            *address = (*address>65535)?*address-65536:*address;
             *address = (*address<0)?*address+65536:*address;
+            //fprintf(stderr,"[%s ,%d]\n",__func__,__LINE__);
             return *address;
         }
     }
@@ -46,16 +56,36 @@ int get_address(int *address){
 
 int main(int argc, char* argv[]){
     int address = rand_int(CM_ADDRESS_SPACE_SIZE);
-    flag = b;
+    if(argc < 2){
+        printf("unenough argument\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "A") == 0) {
+        flag = a;
+        printf("Question A\n");
+        printf("read: %d times\n", READ);
+    } else if (strcmp(argv[1], "B") == 0) {
+        flag = b;
+        printf("Question B\n");
+        printf("read: %d times\n", READ);
+    } else {
+        flag = c;
+    }
     cm_init();
     float acum_time = 0;
 /* cache enable */ 
     cm_enable_cache();
     acum_time = get_statistic_cycle(&address);
-    printf("cache_acum_time: %f\n",acum_time);
+    printf("cache_acum_time: %f cycles\n",acum_time);
+    printf("Hit: %d times\n",hit);
+    printf("Miss: %d times\n",miss);
+    hit = 0;
+    miss =0;
 /* cach unenable */
     cm_disable_cache();
     acum_time = get_statistic_cycle(&address);
-    printf("discache_acum_time: %f\n",acum_time);
+    printf("discache_acum_time: %f cycles\n",acum_time);
+    printf("Hit: %d times\n",hit);
+    printf("Miss: %d times\n",miss);
     return 0;
 }
